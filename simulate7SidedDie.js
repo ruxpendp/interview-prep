f1e97dc54b93a4ff45f4a8e2516e819fcfa460ed
ruxpendp <ruxpendp@gmail.com>
2021-05-11 01:00:40 -0700

    add commentary

--- simulate7SidedDie.js
+++ simulate7SidedDie.js
@@ -24,3 +24,25 @@ for (let i = 0; i < 100000; i++) {
   else results[roll]++;
 }
 console.log(results);
+
+/*--------------------------*/
+
+/*
+* Commentary:
+*
+* My initial instinct was to find some combination of rolls that would yield exactly 35 possible
+* results, 35 being the least common multiple of 5 and 7. This way a range of 5 results would
+* all map to one result on the 7-sided die.
+*
+* The crux for me was realizing that it doesn't matter what probability each result has of
+* occurring, as long as each result has the *same* probabiliy of occurring. Thus we can map
+* any 7 results on a combo of D5 rolls to their respective D7 values, and just throw out/reroll any
+* result that doesn't map to anything.
+*
+* From there, it naturally followed that we'd be wasting a lot of rolls if we threw out all but the
+* first 7 possible results, so taking the modulus allows us to map 3 results to each D7 roll.
+*
+* As for the D5 roll combo, it was pretty clear to me from the start that two rolls nets 5^2
+* possibilities, essentially creating a base-5 number which would need to be converted back to base
+* 10. Important not to just take the sum of D5 rolls, as different sums have different odds.
+*/
--- simulateKSidedDie.js
+++ simulateKSidedDie.js
@@ -8,12 +8,6 @@ const rollN = n => Math.floor(Math.random() * n) + 1;
 // using only rollN(n), returns an integer in the range [1, k] with equal probability
 // assumes n and k are integers larger than 1
 const rollK = (n, k) => {
-  // How many rolls do we need to make?
-  // Every successive rollN() multiplies the total number of possible results by n.
-  // n^x >= k
-  // <=> ln(n^x) >= ln(k)
-  // <=> x * ln(n) >= ln(k)
-  // <=> x >= ln(k) / ln(n)     for positive integers of n
   const rolls = Math.ceil(Math.log(k) / Math.log(n));
 
   const possibleRolls = Math.pow(n, rolls);
@@ -47,3 +41,38 @@ const doRolls = (n, k) => {
 };
 
 PAIRS.forEach(([n, k]) => doRolls(n, k));
+
+/*--------------------------*/
+
+/*
+* Commentary:
+*
+* After solving D5 -> D7 (see simulate7SidedDie.js), of course the real fun part is generalizing
+* to Dn -> Dk.
+*
+* Some extra problems arise, the first one being "How many rolls do we need to make?"
+*
+* We know that every successive Dn roll multiplies the total number of possible results by n.
+* Hence we are solving    n^x >= k    for x.
+*         <=>         ln(n^x) >= ln(k)
+*         <=>        x * ln(n) >= ln(k)
+*         <=>        x >= ln(k) / ln(n)     for positive integers of n
+*
+* If you forgot how you used to manipulate exponents/logarithms like butter in college (I had to
+* look it up too), you can always "brute force" this solution by trying each successive power of n
+* until you got a result that exceeded k. In a real coding interview I probably would've had to
+* take this approach.
+*
+* From there we must calculate the upper bound after which we must throw out/reroll, and create our
+* base-n number and convert to base 10 just like the D5 -> D7 problem.
+*
+* Note that we don't really need any special handling for when n >= k – we're just trying to find a
+* number of Dn rolls that give more possible results than k, then taking the modulus to maximize the
+* number of results we can use. When n >= k, x just becomes 1 and the rest of the problem works the
+* same.
+*
+* Also note that n has to be greater than 1. A 1-sided die only has 1 possible result no matter how
+* many times you reroll. k can technically be 1 but simulating a 1-sided die is... trivial.
+*
+* Super fun problem!
+*/
